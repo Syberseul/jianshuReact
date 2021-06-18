b7a28ca023a2787aeb65f08b78d5c32350491e18
Yiteng Huang <hyy528924543@gmail.com>
2021-06-18 15:35:48 +1000

    use react-redux to refactor the header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Header from "./common/header";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
-  return <Header />;
+  return (
+    <Provider store={store}>
+      <Header />
+    </Provider>
+  );
 }
 
 export default App;
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import { CSSTransition } from "react-transition-group";
 import {
   HeaderWrapper,
@@ -10,56 +11,52 @@ import {
   Button,
 } from "./style";
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      focused: false,
-    };
-    this.handleInputFocus = this.handleInputFocus.bind(this);
-    this.handleInputBlur = this.handleInputBlur.bind(this);
-  }
+const Header = (props) => {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className="left active">Home</NavItem>
+        <NavItem className="left">Download App</NavItem>
+        <NavItem className="right">Log In</NavItem>
+        <NavItem className="right">Aa</NavItem>
+        <CSSTransition in={props.focused} timeout={200} classNames="slide">
+          <NavSearch
+            className={props.focused ? "focused" : ""}
+            onFocus={props.handleInputFocus}
+            onBlur={props.handleInputBlur}
+          ></NavSearch>
+        </CSSTransition>
+      </Nav>
+      <Addition>
+        <Button className="write">Write</Button>
+        <Button className="reg">Sign Up</Button>
+      </Addition>
+    </HeaderWrapper>
+  );
+};
 
-  render() {
-    return (
-      <HeaderWrapper>
-        <Logo />
-        <Nav>
-          <NavItem className="left active">Home</NavItem>
-          <NavItem className="left">Download App</NavItem>
-          <NavItem className="right">Log In</NavItem>
-          <NavItem className="right">Aa</NavItem>
-          <CSSTransition
-            in={this.state.focused}
-            timeout={200}
-            classNames="slide"
-          >
-            <NavSearch
-              className={this.state.focused ? "focused" : ""}
-              onFocus={this.handleInputFocus}
-              onBlur={this.handleInputBlur}
-            ></NavSearch>
-          </CSSTransition>
-        </Nav>
-        <Addition>
-          <Button className="write">Write</Button>
-          <Button className="reg">Sign Up</Button>
-        </Addition>
-      </HeaderWrapper>
-    );
-  }
+const mapStateToProps = (state) => {
+  return {
+    focused: state.focused,
+  };
+};
 
-  handleInputFocus() {
-    this.setState({
-      focused: true,
-    });
-  }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus() {
+      const action = {
+        type: "search_focus",
+      };
+      dispatch(action);
+    },
+    handleInputBlur() {
+      const action = {
+        type: "search_blur",
+      };
+      dispatch(action);
+    },
+  };
+};
 
-  handleInputBlur() {
-    this.setState({
-      focused: false,
-    });
-  }
-}
-
-export default Header;
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
